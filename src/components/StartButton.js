325a2194d28b348b1edfd0368dfdcb1ad46b24f2
Eddie McLean <middlerun@gmail.com>
2018-06-11 18:07:27 +1000

    Add start button logo

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 
 import RidgedButton from '../atoms/RidgedButton'
+
+import logo from '../img/logo-small.png'
 
 const StyledStartButton = RidgedButton.extend`
   padding: 0 3px;
   font-weight: bold;
   white-space: nowrap;
+  image-rendering: pixelated;
+  image-rendering: -moz-crisp-edges;
+`
+
+const LogoImage = styled.img`
+  vertical-align: bottom;
 `
 
 class StartButton extends Component {
@@ -14,7 +23,7 @@ class StartButton extends Component {
 
     return (
       <StyledStartButton className="reactows95-StartButton" onClick={onClick}>
-        <span>🔥 Start</span>
+        <span><LogoImage src={logo}/> Start</span>
       </StyledStartButton>
     )
   }
